25555f26b0f326fc716a7e693120f02ec75e0268
M0hammed-Gamal <m_m20000075@yahoo.com>
2023-07-20 03:16:27 -0700

    hotfix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
 #ifndef STDARG_H
 #define STDARG_H
 #include <stdarg.h>
+#include <stdio.h>
 #endif
 
 #ifndef _VARIADIC_FUNCTIONS_H
@@ -21,10 +22,21 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 void pr_int(va_list);
 void pr_char(va_list);
 void pr_float(va_list);
 void pr_str(va_list);
-#endif
+int _putchar(char c);
 
+typedef struct checker
+{
+	char *type;
+	void (*f)();
+} checker;
+#endif
