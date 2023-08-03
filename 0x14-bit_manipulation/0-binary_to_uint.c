e14868451b54b440ea6a3053c932419b221188c1
M0hammed-Gamal <m_m20000075@yahoo.com>
2023-08-03 16:31:01 -0700

    hot fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,69 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* _strlen - returns the length of a string.
-* @s : pointers
-* Return: val.
-*/
-
-int _strlen(const char *s)
-{
-	int val = 0;
-
-	while (*s != 0)
-	{
-		s++;
-		val++;
-	}
-	return (val);
-}
-
-/**
-* _pow_recursion - returns the value of x raised to the power of y.
-* @x : variable
-* @y : ^variable
-* Return: res
-*/
-
-int _pow_recursion(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
-
-	return (x * _pow_recursion(x, (y - 1)));
-}
-
-/**
-* binary_to_uint - converts a binary number to an unsigned int.
-* @b : pointing to a string of 0 and 1 chars
-* Return: converted number, or 0 if
-*/
-
+ * binary_to_uint - convert a binary number to an unsigned int
+ * @b: char string
+ * Return: converted decimal number or 0 if there is an unconvertable char
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int str = 0, i, j = 0;
+	unsigned int total, power;
+	int len;
 
-	if (b == '\0')
+	if (b == NULL)
 		return (0);
 
-	str = _strlen(b);
-
-	for (i = str - 1 ; i >= 0 ; i--)
+	for (len = 0; b[len]; len++)
 	{
-		if (b[i] == '1' || b[i] == '0')
-		{
-			if (b[i] == '1')
-				num = num + _pow_recursion(2, j);
-		}
-		else
-		{
-			num = 0;
-			return (num);
-		}
-		j++;
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
 	}
-	return (num);
+
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	{
+		if (b[len] == '1')
+			total += power;
+	}
+
+	return (total);
 }
